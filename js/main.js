5564e9c3a4d5656dca866df5f811952367475c27
Davithy <davithy00@gmail.com>
2026-08-30 15:17:06 -0700

    lightbox3 implementation
    Testing out using lightbox3 for my portfolios instead of coding that functionality myself.

---
+++ js/main.js
@@ -0,0 +1,45 @@
+const images = document.querySelectorAll(".gallery-item img");
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modal-img");
+const closeBtn = document.querySelector(".close-btn");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+let imgIndex
+
+images.forEach((img, i) => {
+    img.addEventListener("click", (e) => {
+        imgIndex = i;
+        modalImg.src = images[i].src;
+        modal.classList.add("active");
+    });
+});
+
+closeBtn.onclick = () => {
+    modal.classList.remove("active");
+};
+
+nextBtn.onclick = () => {
+    modalImg.src = nextImg()
+};
+
+prevBtn.onclick = () => {
+    modalImg.src = prevImg();
+};
+
+
+let nextImg = () => {
+    imgIndex++;
+    if (imgIndex >= images.length) {
+        imgIndex = 0;
+    }
+    return images[imgIndex].src;
+};
+
+let prevImg = () => {
+    imgIndex--;
+    if (imgIndex < 0) {
+        imgIndex = images.length - 1
+    }
+    return images[imgIndex].src;
+}
